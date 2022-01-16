d932b2169fddfa65c45439ec8f487112423ed858
mwsolti <85954308+mwsolti@users.noreply.github.com>
2022-01-16 16:15:04 -0300

    Add files via upload

---
+++ Aula Python/ExerciciosAula01.py
@@ -0,0 +1,29 @@
+# #Atividade 1
+
+# nota1 = input("Digite a Nota 1: ")
+# nota2 = input("Digite a Nota 2  ")
+# nota3 = input("Digite a Nota 3  ")
+
+# media = (float(nota1)+float(nota2)+float(nota3))/3
+
+# print(media)
+
+# #Atividade 2
+
+# nome = input("Digite seu Nome: \n")
+
+# sobrenome = input("Digite seu Sobrenome: \n")
+
+# idade = input("Digite sua Idade: \n")
+
+# print("Seu nome é: "+nome+" "+sobrenome+" \n Sua Idade é: "+idade)
+
+#Atividade 3
+
+rua = input("Digita  Rua onde Você Mora: ")
+bairro = input("DIgite o Bairro onde você mora: ")
+cep = input("Digite aseu CEP: ")
+cidade = input("Digite a Cidade onde você mora: ")
+
+
+print("SUa rua é: "+ rua)
---
+++ Aula Python/aula01.py
@@ -0,0 +1,17 @@
+# Exibir Texto na Tela
+print("Bem VIndo Ao Python")
+
+#Coletar dados do usuario
+nome = input("Digite seu Nome: ")
+print(nome)
+#OBS: Toda VAriavel alimentada por input é String
+
+#Interações com Python
+ano = input("Digite o ANo que você Nasceu!  \n")
+
+#Covverssão de Variavel
+idade = 2022-int(ano)
+print(idade)
+
+
+
