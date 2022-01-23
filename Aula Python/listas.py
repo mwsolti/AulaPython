cadf698ada1736d5bd71553aedcef916f4a1a0b3
mwsolti <85954308+mwsolti@users.noreply.github.com>
2022-01-23 14:30:25 -0300

    Add files via upload

---
+++ Aula Python/listas.py
@@ -0,0 +1,21 @@
+
+list_numeros = [0,1,2,3,4,5,6,7,8,9,10]
+
+#Exibe um valor
+print(list_numeros[0])
+
+#Exibe fatiamento de valores
+print(list_numeros[0:2])
+
+#Exibe ultimo valor da lista
+print(list_numeros[-1])
+
+
+
+
+list_nomes = ["João","Beltrano","Cicrano"]
+
+
+
+print(list_numeros)
+
