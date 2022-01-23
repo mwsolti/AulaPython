b868d4beb8343a73a995d6a278599e647a779608
mwsolti <85954308+mwsolti@users.noreply.github.com>
2022-01-23 14:59:16 -0300

    Add files via upload

---
+++ Aula Python/Aula02_ex01.py
@@ -0,0 +1,13 @@
+list_tranquera = ["Martelo sem Cabo","GUarda-Chuva Quebrado","Pedaço de fio de tomada","PAnela Furada","Caixa de Sapato Vazia"]
+
+#Separado
+print(list_tranquera[0])
+print(list_tranquera[-1])
+
+
+#separador
+print("-----------------------------------------------------------------")
+
+
+#Mesmo print
+print(list_tranquera[0]+" \n"+list_tranquera[-1])
---
+++ Aula Python/Aula02_ex02.py
@@ -0,0 +1,6 @@
+#Soma
+list_numeros = [2,2]
+
+soma = list_numeros[0] + list_numeros[1]
+
+print(soma)
