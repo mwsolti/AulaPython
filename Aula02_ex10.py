146d7916eb76432bc89903f096f6efd6a5f1cd74
mwsolti <85954308+mwsolti@users.noreply.github.com>
2022-01-30 14:34:18 -0300

    Delete Aula02_ex10.py

--- Aula02_ex10.py
+++
@@ -1,9 +0,0 @@
-list_alunos = ["Antonieta","Bruno","Zamira","Juriscleudo"]
-
-list_notas_alunos = [[8,7,6],[7,8,5],[10,4,6],[3,4,5]]
-cont = 0
-for list_nota in list_notas_alunos:
-    media = (sum(list_nota))/3
-    print("O aluno: "+list_alunos[cont])
-    print("Tem Media: "+str(media))
-    cont +=1
