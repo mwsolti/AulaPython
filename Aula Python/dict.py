83ad308743ea933d5eb61c80283dd3b253401d56
mwsolti <85954308+mwsolti@users.noreply.github.com>
2022-01-30 14:49:55 -0300

    Add files via upload

---
+++ Aula Python/dict.py
@@ -0,0 +1,20 @@
+#Dicionario
+
+dict_aluno_media = { "JUriscleudo":8, "Antonia":6, "ROberto":4}
+
+#BUsca valor de uma chave do dicionario
+print(dict_aluno_media["Antonia"])
+
+
+#Adciona uma nova chave e valor
+dict_aluno_media["Deiso"] = 10
+
+print(dict_aluno_media)
+
+
+#Mudar calor de chave existente
+
+dict_aluno_media["JUriscleudo"] = 7
+
+
+print(dict_aluno_media)
