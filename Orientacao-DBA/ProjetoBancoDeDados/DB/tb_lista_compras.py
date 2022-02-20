f2d26e8156afcc6b75fa476a5c099e083577d4e5
mwsolti <85954308+mwsolti@users.noreply.github.com>
2022-02-20 18:56:52 -0300

    Add files via upload

---
+++ Orientacao-DBA/ProjetoBancoDeDados/DB/tb_lista_compras.py
@@ -0,0 +1,12 @@
+
+def inser_lista_compras(id_produto,quantidade):
+    dict_produto  = {"id_produtp": id_produto,
+                    "Descricao": quantidade}
+
+def udade_lista_compra(id_produto,quantidade):
+    dict_produto  = {"id_produtp": id_produto,
+                    "Descricao": quantidade}
+
+def select_plista_compra(list_arqumentos):
+    list_produtos = []
+    return list_arqumentos
---
+++ Orientacao-DBA/ProjetoBancoDeDados/DB/tb_produto.py
@@ -0,0 +1,21 @@
+
+
+def inser_produto(nome_produto,ds_produto,preco):
+    dict_produto  = {"Nome": nome_produto,
+                    "Descricao": ds_produto,
+                    "PReco": preco}
+
+
+
+def udade_produto(id,nome_produto,ds_produto,preco):
+    dict_produto  = {"Nome": nome_produto,
+                    "Descricao": ds_produto,
+                    "PReco": preco}
+
+
+def select_produto(list_arqumentos):
+    list_produtos = []
+    return list_arqumentos
+
+
+    #where X =X and Y <> 2
