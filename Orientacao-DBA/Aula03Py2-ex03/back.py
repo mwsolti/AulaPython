cf7ebdb7ae2b18892c3fa5cdeed5bba87ea6042d
mwsolti <85954308+mwsolti@users.noreply.github.com>
2022-02-27 16:34:36 -0300

    Add files via upload

---
+++ Orientacao-DBA/Aula03Py2-ex03/back.py
@@ -0,0 +1,4 @@
+
+def cont_numero(numero):
+    cont_num =  len(str(numero))
+    return cont_num
---
+++ Orientacao-DBA/Aula03Py2-ex03/main.py
@@ -0,0 +1,5 @@
+from back import cont_numero
+
+numero = input("Digite um numero inteiro: ")
+
+print("Quantidade numeros é: "+str(cont_numero(numero)))
